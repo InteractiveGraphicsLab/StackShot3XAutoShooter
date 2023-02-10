ccd39978802885c572bad1a8740392a1d043bd6a
Yuki Yabumoto <fmsnow09@gmail.com>
2023-02-10 13:22:01 +0900

    use action_parser

--- stackshot_util.py
+++ stackshot_util.py
@@ -1,26 +1,28 @@
 from commdefs import *
 from stackshot_controller import StackShotController
+from action_parser import action_parser
 
 
 def start(rawComands: str):
-    commands = []
-    for line in rawComands.splitlines():
-        commands.append(line.split())
-
     # validation
+    try:
+        action_queue = action_parser(rawComands)
+        print(action_queue)
+    except Exception as e:
+        print(e)
 
     try:
         controller = StackShotController()
         controller.open()
 
-        for cmd in commands:
-            if cmd[0] == 'MOVE':
-                if cmd[1] == 'X':
-                    controller.move(RailAxis.COMM_RAIL_AXIS_X, RailDir.COMM_RAIL_DIR_FWD, int(cmd[2]))
-                elif cmd[1] == 'Y':
-                    controller.move(RailAxis.COMM_RAIL_AXIS_Y, RailDir.COMM_RAIL_DIR_FWD, int(cmd[2]))
-                elif cmd[1] == 'Z':
-                    controller.move(RailAxis.COMM_RAIL_AXIS_Z, RailDir.COMM_RAIL_DIR_FWD, int(cmd[2]))
+        for action in action_queue:
+            if action[0] == 'move':
+                if action[1] == 'x':
+                    controller.move(RailAxis.COMM_RAIL_AXIS_X, RailDir.COMM_RAIL_DIR_FWD, int(action[2]))
+                elif action[1] == 'y':
+                    controller.move(RailAxis.COMM_RAIL_AXIS_Y, RailDir.COMM_RAIL_DIR_FWD, int(action[2]))
+                elif action[1] == 'z':
+                    controller.move(RailAxis.COMM_RAIL_AXIS_Z, RailDir.COMM_RAIL_DIR_FWD, int(action[2]))
 
     except Exception as excpt:
         print(excpt)
