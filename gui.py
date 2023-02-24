62ac6db14c7c119d780573e2c6a7f646031abcc4
Yuki Yabumoto <fmsnow09@gmail.com>
2023-02-24 17:40:29 +0900

    implement stop button

--- gui.py
+++ gui.py
@@ -11,10 +11,13 @@ import usb.core
 import subprocess
 import configparser
 
-from multiprocessing import Process
+from PySide6 import QtGui
+from threading import Thread
+from multiprocessing import Value 
 from commdefs import *
 from stackshot_controller import StackShotController
 from action_parser import action_parser
+from exec_actions import exec_actions
 
 
 def isValidBrackets(s, min, max):
@@ -46,7 +49,17 @@ class GUI(QtWidgets.QMainWindow):
         self.config = configparser.ConfigParser()
         self.loadConfig()
 
-        self.current_process = None
+        self.working_thread = None
+        self.stop_flag = Value('i', 0) # 0: false, 1: true
+
+        self.controller = StackShotController()
+        try:
+            print('Connect Stackshot3X')
+            self.controller.open()
+        except Exception as excpt:
+            print(excpt)
+            exit()
+
 
     def loadConfig(self):
         self.config.read('config.ini')
@@ -110,104 +123,29 @@ class GUI(QtWidgets.QMainWindow):
             f.close()
 
     def start(self):
-        self.current_process = Process(target=self.exec_action)
-        self.current_process.start()
-        self.gui.startButton.setEnabled(False)
-        self.gui.startButton.setText('running')
-        self.current_process.join()
-        self.gui.startButton.setEnabled(True)
-        self.gui.startButton.setText('start')
-        self.current_process = None
+        print('start')
+        with self.stop_flag.get_lock():
+            self.stop_flag.value = 0 # false
+        self.working_thread = Thread(target=exec_actions, args=(self.stop_flag, self.gui.actionsText.toPlainText(), self.controller, self.gui.brackets.value(), self.gui.doFocusStacking.isChecked(), self.gui.doMetashape.isChecked(), self.config), daemon=True)
+        self.working_thread.start()
 
     def stop(self):
-        if self.current_process != None:
-            self.current_process.kill()
+        if self.working_thread != None and self.working_thread.is_alive():
+            print('stop')
+            with self.stop_flag.get_lock():
+                self.stop_flag.value = 1 # true
 
-
-    def exec_action(self):
-        raw_actions = self.gui.actionsText.toPlainText()
-        self.stop_flag = False
-
-        # validation
+    def closeEvent(self, event: QtGui.QCloseEvent):
+        if self.working_thread != None and self.working_thread.is_alive():
+            print('stop')
+            with self.stop_flag.get_lock():
+                self.stop_flag.value = 1 # true
+            self.working_thread.join()
         try:
-            action_queue = action_parser(raw_actions)
-            print(action_queue)
-        except Exception as e:
-                print(e)
-                return
-
-        controller = StackShotController()
-        try:
-            controller.open()
-            controller.stop(RailAxis.COMM_RAIL_AXIS_X)
-            controller.stop(RailAxis.COMM_RAIL_AXIS_Y)
-            controller.stop(RailAxis.COMM_RAIL_AXIS_Z)
+            self.controller.stop(RailAxis.COMM_RAIL_AXIS_X)
+            self.controller.stop(RailAxis.COMM_RAIL_AXIS_Y)
+            self.controller.stop(RailAxis.COMM_RAIL_AXIS_Z)
+            self.controller.close()
         except Exception as excpt:
             print(excpt)
             return
-
-        shutter_count = 0
-        tmp_dir = self.gui.imageSrcFolderPath.text() # src folder
-        save_basedir = self.gui.imageSaveFolderPath.text() # save folder
-        brackets = self.gui.brackets.value() # num of brackets
-        # debbug output
-        print("tmp dir:", tmp_dir)
-        print("save basedir", save_basedir)
-        print("brackets:", brackets)
-        try:
-            for action in action_queue:
-                if self.stop_flag == True:
-                    return
-
-                if action[0] == 'move':
-                    if action[1] == 'x':
-                        controller.move(RailAxis.COMM_RAIL_AXIS_X, RailDir.COMM_RAIL_DIR_FWD, float(action[2]))
-                    elif action[1] == 'y':
-                        controller.move(RailAxis.COMM_RAIL_AXIS_Y, RailDir.COMM_RAIL_DIR_FWD, float(action[2]))
-                    elif action[1] == 'z':
-                        controller.move(RailAxis.COMM_RAIL_AXIS_Z, RailDir.COMM_RAIL_DIR_FWD, float(action[2]))
-
-                elif action[0] == 'shutter':
-                    controller.shutter(1, 1., 2.) # NOTE
-
-                    image_paths = [os.path.join(tmp_dir, f) for f in os.listdir(tmp_dir)] # NOTE need ext check
-                    image_paths.sort(key=os.path.getmtime, reverse=True) # desc images timestamp
-                    save_image_paths = image_paths[:brackets]
-
-                    save_dir = os.path.join(save_basedir, 'original', str(shutter_count).zfill(4)) # image save dir
-                    os.makedirs(save_dir) # create image save dir
-                    for image_path in save_image_paths:
-                        image_name = os.path.basename(image_path)
-                        os.rename(image_path, os.path.join(save_dir, image_name)) # move image
-                    shutter_count += 1
-
-        except Exception as excpt:
-            print(excpt)
-
-        finally:
-            controller.close()
-
-        if self.gui.doFocusStacking.isChecked() == True:
-            # focus stacking
-            original_images_dirs = os.listdir(os.path.join(save_basedir, 'original'))
-            os.makedirs(os.path.join(save_basedir, 'stacking')) # create stacking images dir
-            try:
-                for original_dir in original_images_dirs:
-                    if self.stop_flag == True:
-                        return
-
-                    subprocess.run([self.config['general']['heliconfocus_command_path'], \
-                                    '-silent', \
-                                    os.path.join(save_basedir, 'original', original_dir), \
-                                    '-save:' + os.path.join(save_basedir, 'stacking', original_dir + '.jpg'), \
-                                    '-mp:2', \
-                                    '-j:100'], check=True)
-            except Exception as excpt :
-                print(excpt)
-
-        if self.gui.doMetashape.isChecked() == True:
-            # metashape
-            env = os.environ
-            env['IMAGE_PATH'] = os.path.join(save_basedir, 'stacking')
-            env['METASHAPE_PROJECT_PATH'] = self.gui.metashapeProjectFolderPath.text()
-            subprocess.run([self.config['general']['metashape_command_path'], '--gui', '-r', 'metashape_script.py'], env=env)
